471b8095314b54b4a4572d11a1d667c6d7674fc7
Deora <ddrppx@gmail.com>
2023-03-14 18:02:29 -0300

    fixing deleteById ...

--- src/models/dao/userDao.ts
+++ src/models/dao/userDao.ts
@@ -1,5 +1,4 @@
 import User from '../User';
-import { Op } from 'sequelize';
 import { GetAllUserFilters } from './types';
 import { UserInput, UserOutput } from '../User';
 
@@ -7,63 +6,57 @@ export const create = async (new_user_info: UserInput): Promise<UserOutput> => {
 
     const new_user = await User.create(new_user_info);
 
-    // TODO: Solve TS type conflitct
     return new_user;
 }
 
 export const read = async (filters?: GetAllUserFilters): Promise<UserOutput[]> => {
     const read_user = await User.findAll({ logging: false });
     
-    // TODO: Line 17
     return read_user;
 }
 
 export const readSingle = async (user_id: number): Promise<UserOutput> => {
     const read_user = await User.findByPk(user_id);
     if(!read_user){
-        throw new Error('not found');
+        throw new Error('User not found');
     }
 
-    // TODO: Line 17
     return read_user;
 }
 
 export const update = async (user_id: number, userInfo: Partial<UserInput>): Promise<UserOutput> => {
     const user = await User.findByPk(user_id);
-    let result;
 
     if(!user){
-        throw new Error('not found');
+        throw new Error('User not found');
     }
     const updatedUser = await(user as User).update(userInfo)
 
-    // TODO: Line 17
     return updatedUser;
 }
 
 export const deleteById = async (user_id: number): Promise<boolean> => {
     const deletedUser = await User.destroy({
-        where: {
-            id: {user_id}
+        where:{
+            id: user_id
         }
     })
     return !!deletedUser
 }
 
-
-// formatData(users_return: Array<Object[]>): Array<Object[]>{
-//    
-//     let users_formatted = Array<Object[]>;
 //
-//     users_return.forEach(
-//         (user: Object) => {
-//             users_formatted.push({
+// const formatData = (users_return: Array<UserOutput>): Array<UserOutput> => {
+//   
+//     let users: Array<UserOutput> = [];
+//
+//     users_return.forEach((user) => {
+//         users.push({
 //             id: user.id,
 //             firstName: user.firstName,
 //             lastName: user.lastName,
-//             birthday: user.birthday // FORMAT TO Y-m-d
-//         });
-//     });
+//             birthday: user.birthday
+//         })
+//     })
 //
-//     return users_formatted;
+//     return users;
 // }
