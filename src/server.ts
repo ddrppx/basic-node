9a863dec7f6520fc3361f59293b3c2290756bc2b
Deora <ddrppx@gmail.com>
2023-03-14 18:05:18 -0300

    adding dbInit on server start

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import routes from './routes';
+import dbInit from '../database/init';
 
 const app = express();
-
 const port = 6969;
+    // Database sync
+dbInit();
 
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(port, () => {
-    console.log(`Express running on port: ${port}\n   http://localhost:${port}/`);
+    console.log(`Express running on port: ${port}\nLive: http://localhost:${port}/`);
 });
